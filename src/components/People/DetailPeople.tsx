1505f0bf05f543a4b496fbb7905ef6daa2bc495a
melakuabe <Melaku.abebe@sealstone.uk>
2024-08-03 14:07:52 +0300

    chore: Refactor DetailPeople component to improve readability and maintainability

--- src/components/People/DetailPeople.tsx
+++ src/components/People/DetailPeople.tsx
@@ -11,27 +11,24 @@ interface DetailPeopleProps {
 const DetailPeople = ({ name, onClick, place }: DetailPeopleProps) => {
   const { peopleDetailModal, setPeopleDetailModal } = globalStore();
   return (
-    <div>
-      <Handle type="target" position={Position.Top} />
-      <div
-        onClick={() => setPeopleDetailModal(!peopleDetailModal)}
-        className={`grid items-center text-center px-3 py-1.5 border-nodeborder border-r w-6/12 ${
-          peopleDetailModal ? "border border-gray-800 rounded-l-lg" : ""
-        }`}
-      >
-        <span className="block text-black text-8 font-semibold leading-3">
-          {name}
+    <div
+      onClick={() => setPeopleDetailModal(!peopleDetailModal)}
+      className={`grid items-center text-center px-3 py-1.5 border-nodeborder border-r w-6/12 ${
+        peopleDetailModal ? "border border-gray-800 rounded-l-lg" : ""
+      }`}
+    >
+      <span className="block text-black text-8 font-semibold leading-3">
+        {name}
+      </span>
+      <div className="text-center block -mt-1.5">
+        <span className="inline-flex items-center text-nodetx text-8">
+          <img
+            src="https://flagicons.lipis.dev/flags/4x3/it.svg"
+            alt=""
+            className="h-1.5 px-1"
+          />
+          {place}
         </span>
-        <div className="text-center block -mt-1.5">
-          <span className="inline-flex items-center text-nodetx text-8">
-            <img
-              src="https://flagicons.lipis.dev/flags/4x3/it.svg"
-              alt=""
-              className="h-1.5 px-1"
-            />
-            {place}
-          </span>
-        </div>
       </div>
     </div>
   );
